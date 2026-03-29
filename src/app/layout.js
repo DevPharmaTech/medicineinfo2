0883cdf1a7e43f9bd1418ffa27b3d3e6c091b411
Divyanshu Singh <divyanshusingh180203@gmail.com>
2026-03-29 15:19:50 +0530

    feat: implement root layout with Geist fonts, Vercel Analytics, and Speed Insights integration

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { appConfig } from "@/config/appConfig";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Analytics />
+        <SpeedInsights />
         {children}
       </body>
     </html>
